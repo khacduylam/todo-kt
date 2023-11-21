e0dc0dce94f51e2d7e72262d931a9c63bc90c90a
duy <lamkhacduy13@gmail.com>
2023-11-21 14:48:18 +0700

    initial commit

---
+++ src/main/kotlin/com/ttavyud/todokt/TodoktApplication.kt
@@ -0,0 +1,11 @@
+package com.ttavyud.todokt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoktApplication
+
+fun main(args: Array<String>) {
+	runApplication<TodoktApplication>(*args)
+}
---
+++ src/test/kotlin/com/ttavyud/todokt/TodoktApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ttavyud.todokt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoktApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
